5e01114f866d1ffa2410bd2d74eea3fb3a5546c8
shizuwei <shizuwei99@gmail.com>
2021-09-10 11:22:38 +0800

    2021/9/10 shizuwei 双指针有序数组优化

--- Tran018-doublePoint/src/demo1/Mian.java
+++ Tran018-doublePoint/src/demo1/Mian.java
@@ -8,7 +8,7 @@ package demo1;
 public class Mian {
 
     public static void main(String[] args) {
-        int[] nums = {-1};
+        int[] nums = {-1,0,3,4,6};
         int[] squares = sortedSquares(nums);
         for (int i : squares) {
             System.out.print(i+",");
@@ -21,31 +21,57 @@ public class Mian {
      * @return int[]
      * @Author: shizuwei 2021/8/28 8:28
      */
+//    public static int[] sortedSquares(int[] nums) {
+//        int neg = nums.length;      // 第一个非负数
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] >= 0) {
+//                neg = i;
+//                break;
+//            }
+//        }
+//
+//        int left = neg - 1, right = neg, index = 0;
+//        int[] ans = new int[nums.length];
+//        while (left >= 0 || right < nums.length) {
+//            if (left < 0) {
+//                ans[index++] = nums[right] * nums[right++];
+//            }else if (right == nums.length) {
+//                ans[index++] = nums[left] * nums[left--];
+//            }else if (nums[left] * nums[left] < nums[right] * nums[right]) {
+//                ans[index++] = nums[left] * nums[left--];
+//            } else {
+//                ans[index++] = nums[right] * nums[right++];
+//            }
+//
+//        }
+//        return ans;
+//    }
+
+
+
     public static int[] sortedSquares(int[] nums) {
-        int neg = nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= 0) {
-                neg = i;
-                break;
+        int n = nums.length;
+        int[] ans = new int[n];
+//        for (int i = 0, j = n - 1, pos = n - 1; i <= j;) {
+//            if (nums[i] * nums[i] > nums[j] * nums[j]) {
+//                ans[pos] = nums[i] * nums[i];
+//                ++i;
+//            } else {
+//                ans[pos] = nums[j] * nums[j];
+//                --j;
+//            }
+//            --pos;
+//        }
+        int i = 0, j = n - 1, index = n - 1;
+        while (i <= j) {
+            if (nums[i] * nums[i] > nums[j] * nums[j]) {
+                ans[index] = nums[i] * nums[i];
+                ++i;
+            } else {
+                ans[index] = nums[j] * nums[j];
+                --j;
             }
-        }
-
-        int left = neg - 1, right = neg, index = 0;
-        int[] ans = new int[nums.length];
-        while (left >= 0 && right < nums.length) {
-            if (nums[left] * nums[left] < nums[right] * nums[right]) {
-                ans[index++] = nums[left] * nums[left--];
-            }else {
-                ans[index++] = nums[right] * nums[right++];
-            }
-        }
-
-        while (left >= 0) {
-            ans[index++] = nums[left] * nums[left--];
-        }
-
-        while (right < nums.length) {
-            ans[index++] = nums[right] * nums[right++];
+            --index;
         }
         return ans;
     }
