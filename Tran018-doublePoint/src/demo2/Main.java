340da11aedee128efc6274b4b8a0444e33074d3e
shizuwei <shizuwei99@gmail.com>
2021-09-11 11:07:09 +0800

    2021/09/11 shizuwei 旋转数组

--- Tran018-doublePoint/src/demo2/Main.java
+++ Tran018-doublePoint/src/demo2/Main.java
@@ -9,21 +9,44 @@ import java.util.Arrays;
  **/
 public class Main {
 
+
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 2;
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
         new Main().rotate(nums, k);
-        Arrays.stream(nums).forEach(item-> System.out.print(item));
+        Arrays.stream(nums).forEach(item -> System.out.print(item + " "));
     }
 
     /**
      * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     *
      * @param nums
      * @param k
      * @return void
-     * @Author: shizuwei 2021/8/28 9:05
+     * @author: Shi Zuwei on 2021/9/11 10:37
      */
     public void rotate(int[] nums, int k) {
-        
+        reverseArr(nums, 0, nums.length - 1);
+        reverseArr(nums, 0, k%nums.length-1 );
+        reverseArr(nums, k%nums.length, nums.length - 1);
+    }
+
+    /**
+     * 反转数组元素
+     *
+     * @param nums
+     * @param start
+     * @param end
+     * @return void
+     * @author: Shi Zuwei on 2021/9/11 10:53
+     */
+    private void reverseArr(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
