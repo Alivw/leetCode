eefbe4496ea5f5ad224f50352c825e36a5928e58
shizuwei <shizuwei99@gmail.com>
2021-11-12 16:46:16 +0800

    2021-11-12 最大子序和

--- Tran020-dynamicProgramming/src/不同路径/Main.java
+++ Tran020-dynamicProgramming/src/不同路径/Main.java
@@ -40,6 +40,5 @@ public class Main {
             }
         }
         return f[m - 1][n - 1];
-
     }
 }
--- Tran020-dynamicProgramming/src/数组最大子序和/Main.java
+++ Tran020-dynamicProgramming/src/数组最大子序和/Main.java
@@ -1,4 +1,4 @@
-package 最大自序和;
+package 数组最大子序和;
 
 /**
  * @Description: 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
@@ -27,13 +27,11 @@ public class Main {
          * dp[0]=num[0];
          */
         int length = nums.length, max = nums[0];
-        int[] dp = new int[length];
-        dp[0] = nums[0];
         for (int i = 1; i < length; i++) {
             //转移公式
-            dp[i] = Math.max(dp[i - 1], 0) + nums[i];
+            nums[i] = Math.max(nums[i - 1], 0) + nums[i];
             //记录最大值
-            max = Math.max(max, dp[i]);
+            max = Math.max(max, nums[i]);
         }
         return max;
     }
