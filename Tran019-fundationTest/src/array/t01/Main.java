11c2ea0c421379456b03c3511b5b65bbd931944f
shizuwei <shizuwei99@gmail.com>
2021-11-26 16:22:32 +0800

    相同的树

--- Tran019-fundationTest/src/array/t01/Main.java
+++ Tran019-fundationTest/src/array/t01/Main.java
@@ -6,4 +6,28 @@ package array.t01;
  * @Create: 2021-09-11 17:08
  **/
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(strStr("mississippi", "mississippi"));
+    }
+
+
+
+
+    public static int strStr(String haystack, String needle) {
+        int m =haystack.length(),n=needle.length();
+        for (int i = 0; i+n<=m; i++) {
+            boolean flag=true;
+            for (int j =0; j < n; j++) {
+                if (haystack.charAt(i) != needle.charAt(j)){
+                    flag=false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
---
+++ Tran023-tree/src/相同的树/Main.java
@@ -0,0 +1,39 @@
+package 相同的树;
+
+/**
+ * @Author: Alivv
+ * @Create: 2021-11-26 16:21
+ **/
+public class Main {
+
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+
+        if (p == null || q == null) return false;
+
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
