2611a669d68ce8f9ba55e74e95dd36eeff61b13e
jalivv <shizuwei99@gmail.com>
2021-12-16 15:55:46 +0800

    最长回文字串

--- Tran014-2LinkListToInt/src/demo/Main.java
+++ Tran014-2LinkListToInt/src/demo/Main.java
@@ -1,3 +1,5 @@
+package demo;
+
 /**
  * @Description: 二进制链表转换为整数
  * 给你一个单链表的引用结点head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
@@ -11,7 +13,7 @@ public class Main {
         ListNode head = new ListNode();
         ListNode list = head;
         for (int i = 0; i < num.length(); i++) {
-            list.next = new ListNode((int) num.charAt(i)-48);  // '1' ascii=49
+            list.next = new ListNode((int) num.charAt(i) - 48);  // '1' ascii=49
             list = list.next;
         }
         System.out.println(getDecimalValue(head));
---
+++ Tran024-maxTriangleLength/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+/**
+ * @Description TODO
+ * @Author jalivv
+ * @Date 2021/12/11 09:28
+ */
+public class Main {
+
+    /**
+     * 给定由一些正数（代表长度）组成的数组 A，返回由其中三个长度组成的、面积不为零的三角形的最大周长。
+     *
+     * 如果不能形成任何面积不为零的三角形，返回 0。
+     */
+
+    public static void main(String[] args) {
+        System.out.println("组成最长的三角形周长为:" + largestPerimeter(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
+    }
+
+    public static int largestPerimeter(int[] nums) {
+
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i] < nums[i - 1] + nums[i - 2]) {
+                return nums[i] + nums[i - 1] + nums[i - 2];
+            }
+        }
+        return 0;
+    }
+}
