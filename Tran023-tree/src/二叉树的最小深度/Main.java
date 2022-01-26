029a892d5f1fcc54f17fc9f7c6cfb2537892dcf5
jalivv <shizuwei99@gmail.com>
2022-01-26 22:52:03 +0800

    还是没写出来

--- Tran023-tree/src/二叉树的最小深度/Main.java
+++ Tran023-tree/src/二叉树的最小深度/Main.java
@@ -12,6 +12,11 @@ import java.util.Queue;
  */
 public class Main {
 
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(2, null, new TreeNode(3, null, new TreeNode(4, null, new TreeNode(5, null, new TreeNode(6, null, null)))));
+        TreeNode node = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, null, null));
+        System.out.println(new Main().minDepthBFS(root1));
+    }
 
     public int minDepth(TreeNode root) {
         if(root==null){
@@ -32,26 +37,53 @@ public class Main {
      * @return
      */
     public int minDepthBFS(TreeNode root) {
+        return minDepthBFS(root, new LinkedList<>());
     }
 
-    private int minDepthBFS(TreeNode root, Queue<TreeNode> queue, int minDepth ) {
+    private int minDepthBFS(TreeNode root, Queue<TreeNode> queue) {
         if (root == null) {
             return 0;
         }
+        int index = 0;      // 树的第几层
         if (root.left != null) {
+            index++;
             queue.add(root.left);
         }
-        if (root.left != null) {
+        if (root.right != null) {
+            index++;
             queue.add(root.right);
         }
-        minDepth=1;
-        TreeNode node = new TreeNode();
-        while ( (node = queue.remove() )!= null) {
-            if (node.left == null && node.right == null) {
-                return minDepth;
+        TreeNode node = null;
+        int minDepth = 1;
+        while (!queue.isEmpty()) {
+
+            while (index != 0&&!queue.isEmpty()) {
+                node = queue.remove();
+                if (node.left == null && node.right == null) {
+                    return minDepth + 1;
+                }
+                if (node!=null&& node.left != null) {
+
+                    queue.add(node.left);
+                }
+                if (node!=null&&node.right != null) {
+                    queue.add(node.right);
+                }
+                index--;
             }
 
+            if (node!=null&& node.left != null) {
+
+                index++;
+            }
+            if (node!=null&&node.right != null) {
+
+                index++;
+            }
+            minDepth++;
+
         }
+        return minDepth;
     }
 }
 
