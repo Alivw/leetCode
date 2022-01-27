7c64ba7bd865921f6b9512c2f19faa74b2b1ace1
jalivv <shizuwei99@gmail.com>
2022-01-27 09:10:28 +0800

    基于广度优先搜索 获取二叉树最小深度

--- Tran023-tree/src/二叉树的最小深度/Main.java
+++ Tran023-tree/src/二叉树的最小深度/Main.java
@@ -19,20 +19,21 @@ public class Main {
     }
 
     public int minDepth(TreeNode root) {
-        if(root==null){
+        if (root == null) {
             return 0;
         }
-        if(root.left==null){
-            return minDepth(root.right)+1;
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
         }
-        if(root.right==null){
-            return minDepth(root.left)+1;
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
         }
-        return Math.min(minDepth(root.left)+1,minDepth(root.right)+1);
+        return Math.min(minDepth(root.left) + 1, minDepth(root.right) + 1);
     }
 
     /**
      * 基于广度优先搜索获取二叉树的最小深度
+     *
      * @param root
      * @return
      */
@@ -57,26 +58,26 @@ public class Main {
         int minDepth = 1;
         while (!queue.isEmpty()) {
 
-            while (index != 0&&!queue.isEmpty()) {
+            while (index != 0 && !queue.isEmpty()) {
                 node = queue.remove();
                 if (node.left == null && node.right == null) {
                     return minDepth + 1;
                 }
-                if (node!=null&& node.left != null) {
+                if (node != null && node.left != null) {
 
                     queue.add(node.left);
                 }
-                if (node!=null&&node.right != null) {
+                if (node != null && node.right != null) {
                     queue.add(node.right);
                 }
                 index--;
             }
 
-            if (node!=null&& node.left != null) {
+            if (node != null && node.left != null) {
 
                 index++;
             }
-            if (node!=null&&node.right != null) {
+            if (node != null && node.right != null) {
 
                 index++;
             }
@@ -85,18 +86,82 @@ public class Main {
         }
         return minDepth;
     }
+
+    /**
+     * 基于广度优先搜索 获取二叉树的最小深度
+     * @param root
+     * @param a
+     * @return
+     */
+    private int minDepthBFS(TreeNode root, Integer a) {
+        Queue<TreeNodeDepth> queue = new LinkedList<>();
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left != null) {
+            queue.add(new TreeNodeDepth(root.left, 2));
+        }
+        if (root.right!=null) {
+            queue.add(new TreeNodeDepth(root.right, 2));
+        }
+
+        while (!queue.isEmpty()) {
+            TreeNodeDepth ndepth = queue.remove();
+            TreeNode node = ndepth.node;
+            int depth = ndepth.depth;
+            if (node.left == null && node.right == null) {
+                return ndepth.depth;
+            }
+            if (node.left != null) {
+                queue.add(new TreeNodeDepth(node.left, depth + 1));
+            }
+
+            if (node.right != null) {
+                queue.add(new TreeNodeDepth(node.right, depth + 1));
+            }
+        }
+        return 1;
+    }
 }
 
 
-  class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+/**
+ * 树型结构中的每个节点
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+/**
+ * 包装节点跟该节点的深度的类
+ */
+class TreeNodeDepth {
+    TreeNode node;
+
+    int depth;
+
+    public TreeNodeDepth() {
+    }
+
+    public TreeNodeDepth(TreeNode node, int depth) {
+        this.node = node;
+        this.depth = depth;
+    }
+}
