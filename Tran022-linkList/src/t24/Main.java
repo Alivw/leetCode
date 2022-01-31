ae3c42da4090ce63477c8ce9650cf8b5bc93d1f9
jalivv <shizuwei99@gmail.com>
2022-01-31 11:52:02 +0800

    两两交换链表中的节点-迭代实现

--- Tran022-linkList/src/t24/Main.java
+++ Tran022-linkList/src/t24/Main.java
@@ -14,7 +14,11 @@ public class Main {
 
     }
 
-
+    /**
+     * 递归实现
+     * @param head
+     * @return
+     */
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) {
             return head;
@@ -24,6 +28,28 @@ public class Main {
         newHead.next = head;
         return newHead;
     }
+
+
+    /**
+     * 迭代实现
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs1(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+
 }
 
 
