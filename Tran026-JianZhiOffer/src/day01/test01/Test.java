9c5b0b133e60b9a1193f9209b70fc0eaf310ea31
jalivv <shizuwei99@gmail.com>
2022-02-05 20:38:10 +0800

    两个栈实现队列

---
+++ Tran026-JianZhiOffer/src/day01/test01/Test.java
@@ -0,0 +1,51 @@
+package day01.test01;
+
+import java.util.Stack;
+
+/**
+ * @Description
+ * @Created: with IntelliJ IDEA.
+ * @Author jalivv
+ * @createTime 2022/2/5 20:31
+ */
+public class Test {
+}
+
+/**
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead操作返回 -1 )
+ *
+ */
+class CQueue {
+
+    private Stack<Integer> stack1;
+
+    private Stack<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if (!stack2.isEmpty()) {
+            return stack2.pop();
+        }else {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.isEmpty() ? -1 : stack2.pop();
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
