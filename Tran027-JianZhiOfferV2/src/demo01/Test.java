e1bf5d4eaf62a054b476c427b892a3c22b3c77e6
jalivv <shizuwei99@gmail.com>
2022-02-26 13:41:28 +0800

    剑指 Offer 32 - III. 从上到下打印二叉树 III

--- Tran027-JianZhiOfferV2/src/demo01/Test.java
+++ Tran027-JianZhiOfferV2/src/demo01/Test.java
@@ -1,8 +1,17 @@
 package demo01;
 
+import com.apple.eawt.AppEvent;
+import jdk.jfr.internal.WriteableUserPath;
+
+import java.time.temporal.Temporal;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Test {
     public static void main(String[] args) {
-        new Test().exchange(new int[]{1,11,14});
+        new Test().exchange(new int[]{1, 11, 14});
     }
 
 
@@ -42,7 +51,7 @@ public class Test {
             return false;
         }
 
-        if(k==worlds.length-1) return true;
+        if (k == worlds.length - 1) return true;
         board[i][j] = '\0';
         boolean ans = dfs(board, worlds, i, j + 1, k + 1) || dfs(board, worlds, i + 1, j, k + 1) ||
                 dfs(board, worlds, i - 1, j, k + 1) || dfs(board, worlds, i, j - 1, k + 1);
@@ -53,21 +62,68 @@ public class Test {
     /**
      * 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
      * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数在数组的前半部分，所有偶数在数组的后半部分。
-     * @param nums
-     * [1,2,3,4]
+     *
+     * @param nums [1,2,3,4]
      * @return
      */
     public int[] exchange(int[] nums) {
-        int i =0,j=nums.length-1;
-        while (i<j){
-            while (i<j&&(nums[i]&1)==1) i++;
-            while (i<j&&(nums[j]&1)==0) j--;
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            while (i < j && (nums[i] & 1) == 1) i++;
+            while (i < j && (nums[j] & 1) == 0) j--;
             int temp = nums[i];
             nums[i] = nums[j];
-            nums[j]=temp;
+            nums[j] = temp;
         }
         return nums;
     }
 
 
+    /**
+     * 剑指 Offer 32 - III. 从上到下打印二叉树 III
+     * 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            //LinkedList<Integer> tmp = new LinkedList<>();
+            //for(int i = queue.size(); i > 0; i--) {
+            //    TreeNode node = queue.poll();
+            //    if(res.size() % 2 == 0) tmp.addLast(node.val); // 偶数层 -> 队列头部
+            //    else tmp.addFirst(node.val); // 奇数层 -> 队列尾部
+            //    if(node.left != null) queue.add(node.left);
+            //    if(node.right != null) queue.add(node.right);
+            //}
+            List<Integer> temp = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+
+
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
 }
